7fdeceb3c7564ffc84f6782024ed77ef8a89e28e
Francisco Maria Calisto <francisco.mcalisto@gmail.com>
2016-06-03 23:36:42 +0100

    [FINISH] Threshold Function

--- src/recognizer.py
+++ src/recognizer.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+np.seterr(over='ignore')
+
 # img = Image.open('images/img2.png')
 
 # an array that corresponds into 'img' image
@@ -26,12 +28,53 @@ import time
 def threshhold(imageArrayArg):
 
     balanceArray = []
+
+
     newArray = imageArrayArg
 
     for imageRow in imageArrayArg:
         for imagePix in imageRow:
 
-            print imagePix
+            imagePixSize = len(imagePix[:3])
+            imagePixApply = lambda x, y: x + y
+            imagePixReduce = reduce(lambda x, y: x + y, imagePix[:3])
+            imagePixDiv = imagePixReduce / imagePixSize
+
+            avgNum = imagePixDiv
+
+            balanceArray.append(avgNum)
+
+    balanceArraySize = len(balanceArray)
+    balanceArrayApply = lambda x, y: x + y
+    balanceArrayReduce = reduce(lambda x, y: x + y, balanceArray)
+    balanceArrayDiv = balanceArrayReduce / balanceArraySize
+
+    balance = balanceArrayDiv
+
+    for imageRow in newArray:
+        for imagePix in imageRow:
+
+            imagePixSize = len(imagePix[:3])
+            imagePixApply = lambda x, y: x + y
+            imagePixReduce = reduce(lambda x, y: x + y, imagePix[:3])
+            imagePixDiv = imagePixReduce / imagePixSize
+
+            avgNum = imagePixDiv
+
+            if avgNum > balance:
+                imagePix[0] = 255
+                imagePix[1] = 255
+                imagePix[2] = 255
+                imagePix[3] = 255
+
+            else:
+                imagePix[0] = 0
+                imagePix[1] = 0
+                imagePix[2] = 0
+                imagePix[3] = 255
+
+    return newArray
+
 
 
 image1 = Image.open('images/img2.png')
@@ -44,11 +87,11 @@ threshhold(imageArray2)
 
 # Position of the matplot windows
 
-'''fig = plt.figure()
+fig = plt.figure()
 aux1 = plt.subplot2grid((8, 6), (1, 0), rowspan = 10, colspan = 3)
 aux2 = plt.subplot2grid((8, 6), (1, 3), rowspan = 10, colspan = 3)
 
 aux1.imshow(imageArray1)
 aux2.imshow(imageArray2)
 
-plt.show()'''
+plt.show()
